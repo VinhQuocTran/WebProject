52a1247c44921301f45b26ed08bc4714ebba3410
VinhQuocTran <vinhquoc2103@gmail.com>
2023-02-08 10:02:50 +0700

    Update connection.php

--- api/connection.php
+++ api/connection.php
@@ -7,7 +7,7 @@ $username = 'root';
 $password = '';
 
 try {
-    $dbCon = new PDO("mysql:host=" . $host . ";dbname=" . $dbName, $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
+    $dbCon = new PDO("mysql:host=" . $host . ";port=3307;dbname=" . $dbName, $username, $password, array(PDO::ATTR_ERRMODE => PDO::ERRMODE_EXCEPTION));
 } catch (PDOException $ex) {
     die(json_encode(array('status' => false, 'data' => 'Unable to connect: ' . $ex->getMessage())));
 }
